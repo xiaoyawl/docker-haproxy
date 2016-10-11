d150b99d40d2dbc8c39e5a65d0caf6b082f93243
Benyoo <admin@dwhd.org>
2016-10-11 20:20:00 +0800

    haproxy run in docker first update

---
+++ docker-haproxy-alpine/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#########################################################################
+# File Name: entrypoint.sh
+# Author: LookBack
+# Email: admin#dwhd.org
+# Version:
+# Created Time: 2016年10月01日 星期六 09时28分45秒
+#########################################################################
+
+set -e
+[[ $DEBUG == true ]] && set -x
+
+if [ -n "$TIMEZONE" ]; then
+	rm -rf /etc/localtime && \
+	ln -s /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+fi
+
+#[ "${1:0:1}" = '-' ] && set -- nginx "$@"
+[ ! -d /var/log/supervisor ] && mkdir -p /var/log/supervisor
+supervisord -n -c /etc/supervisord.conf
