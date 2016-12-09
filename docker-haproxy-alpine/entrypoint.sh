5b5bc0717bcf0d3df38a04828f623804ebec93cb
Legion-LookBack-Ben-Yoo <taoxiaoyuzy@vip.qq.com>
2016-12-09 20:13:00 +0800

    Update entrypoint.sh

--- docker-haproxy-alpine/entrypoint.sh
+++ docker-haproxy-alpine/entrypoint.sh
@@ -15,6 +15,8 @@ if [ -n "$TIMEZONE" ]; then
 	ln -s /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 fi
 
+[[ -n "${SUPERVISORD_PORT}" ]] && sed -i "s/^port.*/port 0.0.0.0:$SUPERVISORD_PORT/" /etc/supervisord.conf
+
 #[ "${1:0:1}" = '-' ] && set -- nginx "$@"
 [ ! -d /var/log/supervisor ] && mkdir -p /var/log/supervisor
 supervisord -n -c /etc/supervisord.conf
